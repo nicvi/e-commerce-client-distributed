904354d58591e8a748ea2ba643cf976a3916eec7
Herman Pineda <hnpineda@espol.edu.ec>
2021-07-31 23:12:51 -0500

    Uploading the original client app from which the improvements will be made to create the client app distributed

---
+++ app/src/main/java/com/example/elv_commerce/BillViewModel_K.kt
@@ -0,0 +1,23 @@
+package com.example.elv_commerce
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+class BillViewModel_K class MainViewModel : ViewModel() {
+
+    /*
+        val flow = Pager(PagingConfig(20)) {
+            FirestorePagingSource(
+                    FirebaseFirestore.getInstance(),
+                    FirebaseAuth.getInstance().getCurrentUser()
+            )
+        }.flow.cachedIn(viewModelScope)
+
+     */
+
+    }
---
+++ app/src/main/java/com/example/elv_commerce/activityBill.kt
@@ -0,0 +1,9 @@
+package com.example.elv_commerce
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class activityBill  : AppCompatActivity() {
+
+
+}
